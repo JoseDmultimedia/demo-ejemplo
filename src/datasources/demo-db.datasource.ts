5719ec501e64be069ea8703025c20465151cca1e
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2022-04-29 11:03:56 -0500

    Adding lines to config env variables for docker

--- src/datasources/demo-db.datasource.ts
+++ src/datasources/demo-db.datasource.ts
@@ -4,12 +4,11 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: 'demoDb',
   connector: 'mysql',
-  url: '',
-  host: 'host.docker.internal',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'demoDb'
+  host: process.env.DB_HOST ?? '',
+  port: process.env.DB_PORT ?? '',
+  user: process.env.DB_USER ?? '',
+  password: process.env.DB_PASSWORD ?? '',
+  database: process.env.DB_DATABASE ?? ''
 };
 
 // Observe application's life cycle to disconnect the datasource when
